cfdf9e81683dd25df591116a7dce0e5c1cd355bf
Mykola Krysiuk <mkrysiuk@lohika.com>
2019-11-12 11:31:58 +0200

    add insert and select impl, add test

--- repl.py
+++ repl.py
@@ -1,6 +1,29 @@
 import cmd
 import sys
+from dataclasses import dataclass
 
+
+@dataclass
+class TableRowDef:
+    id: int
+    username: str
+    email: str
+
+class InmemTable:
+    __row_def = TableRowDef
+
+    def __init__(self):
+        self.__rows = []
+
+    def insert(self, insert_args: tuple):
+        self.__rows.append(self.__row_def(*insert_args))
+
+    def select(self):
+        for row in self.__rows:
+            print(row)
+
+    def clear_table(self):
+        self.__rows = []
 
 class REPL(cmd.Cmd):
     metacommands = ("exit",)
@@ -9,6 +32,7 @@ class REPL(cmd.Cmd):
     meta_prefix = "."
     intro = "Welcome to the pysql shell. Type help or ? to list commands.\n"
     prompt = "(pysql) "
+    table = InmemTable()
 
     def do_exit(self, arg):
         "Exits the REPL"
@@ -37,8 +61,14 @@ class REPL(cmd.Cmd):
     def parse_statement(self, line):
         if line.startswith(self.select_statement):
             print(f"Executing select statement {line}")
+            self.table.select()
         elif line.startswith(self.insert_statement):
             print(f"Executing insert statement {line}")
+            insert_args = line.split()[1:]
+            if len(insert_args) != 3:
+                raise ValueError("Invalid insert parameter")
+            row = (int(insert_args[0]), *insert_args[1:])
+            self.table.insert(row)
         else:
             print(f"Unrecognized statement {line}")
 
---
+++ test_repl.py
@@ -0,0 +1,27 @@
+import pexpect
+from repl import TableRowDef
+import pytest
+
+REPL_PROMPT = "\(pysql\) "
+
+
+@pytest.fixture(autouse=True)
+def child():
+    child_p = pexpect.spawn(".venv/bin/python repl.py", timeout=5)
+    child_p.expect(REPL_PROMPT)
+    yield child_p
+    child_p.kill(-9)
+
+
+def test_insert(child):
+    insert_line = "insert 1 aaa bbb"
+    # import ipdb; ipdb.set_trace()
+    child.sendline(insert_line)
+    # child.expect("Executing insert statement " + insert_line)
+    child.expect(REPL_PROMPT)
+    select_line = "select"
+    child.sendline(select_line)
+    child.expect(REPL_PROMPT)
+    # expected_row = repr(TableRowDef(1, "aaa", "bbb"))
+    # child.expect("Executing select statement " + select_line)
+    # child.expect(expected_row)
